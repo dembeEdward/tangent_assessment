cb3e4a6ca0aac1ca69f9e1ba8ca2999345930c1a
Edward <dembe.tshifaro@gmail.com>
2016-07-12 14:41:35 +0200

    2017-07-12

--- tangent-assessment/tests/Pivotal-angular.controller.test.js
+++ tangent-assessment/tests/Pivotal-angular.controller.test.js
@@ -1,11 +1,15 @@
 describe('Pivotal-angular', function(){
 
   beforeEach(module('pivotalAngular'));
+  //inject the currentIteration services
+  var currentIteration;
 
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
+  beforeEach(inject(function($service){
+    currentIteration = $service('currentIteration', {});
   }));
+
+  it('should have a working currentIteration service', function(){
+    currentIteration.toBeDefined();
+  });
 
 });
