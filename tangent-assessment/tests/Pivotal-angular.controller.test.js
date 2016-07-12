84e902478d38e477d28d88bd6aad71558e0ad70e
Edward <dembe.tshifaro@gmail.com>
2016-07-12 16:14:53 +0200

    2017-07-12

--- tangent-assessment/tests/Pivotal-angular.controller.test.js
+++ tangent-assessment/tests/Pivotal-angular.controller.test.js
@@ -1,15 +1,30 @@
 describe('Pivotal-angular', function(){
 
-  beforeEach(module('pivotalAngular'));
-  //inject the currentIteration services
+  beforeEach(function(){
+    module('pivotalAngular');
+    //module('services');
+    //module('controllers');
+    //module('services');
+  });
   var currentIteration;
 
-  beforeEach(inject(function($service){
-    currentIteration = $service('currentIteration', {});
+  beforeEach(inject(function($injector){
+    currentIteration = $injector.get('currentIteration');
+    $rootScope = $injector.get('$rootScope');
   }));
 
-  it('should have a working currentIteration service', function(){
-    currentIteration.toBeDefined();
+  it('currentIteration service should be defined', function(){
+      expect(currentIteration).toBeDefined();
+  });
+
+  it('currentIteration service should be working', function(){
+    var scope = "current";
+    //should not return a null when scope is current
+    expect(currentIteration.getCurrentIteration(scope)).not.toBe(null);
+    console.log(currentIteration.getCurrentIteration("soso").$$state);
+    //should not return a status 200 scope is current
+    //expect(currentIteration.getCurrentIteration(scope).status).toBe(200);
+
   });
 
 });
